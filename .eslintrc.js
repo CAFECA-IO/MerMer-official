93056a58450b89d72cc88edd0fc22c7b421d04ce
Julian <julian890701@gmail.com>
2023-06-26 17:02:23 +0800

    set tailwind

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:import/recommended',
+        'plugin:tailwindcss/recommended',
       ],
       parser: '@typescript-eslint/parser',
       plugins: ['@typescript-eslint'],
@@ -24,7 +25,25 @@ module.exports = {
   // 加上 no console log 規則
   rules: {
     'no-console': 'error',
+    'tailwindcss/no-contradicting-classname': 'error',
+    'tailwindcss/classnames-order': 'off',
+    'tailwindcss/enforces-negative-arbitrary-values': 'error',
+    'tailwindcss/enforces-shorthand': 'off',
+    'tailwindcss/migration-from-tailwind-2': 'error',
+    'tailwindcss/no-arbitrary-value': 'error',
+    'tailwindcss/no-custom-classname': 'error',
   },
   // 整合 prettier 和解決 prettier 衝突問題
   plugins: ['prettier'],
+  settings: {
+    tailwindcss: {
+      // These are the default values but feel free to customize
+      callees: ['classnames', 'clsx', 'ctl'],
+      config: 'tailwind.config.js',
+      cssFiles: ['**/*.css', '!**/node_modules', '!**/.*', '!**/dist', '!**/build'],
+      cssFilesRefreshRate: '5_000',
+      removeDuplicates: true,
+      whitelist: [],
+    },
+  },
 };
