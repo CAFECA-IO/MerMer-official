593c8b874a7b5d6f82d86dde7e3a58eec6167310
Julian <julian890701@gmail.com>
2023-06-27 17:05:57 +0800

    fix css

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
 
   return (
     <footer className="flex flex-col h-auto w-screen px-4 py-12 space-y-16 items-center bg-mermerTheme font-Dosis text-sm lg:py-6 lg:px-20 lg:flex-row lg:space-y-0 lg:h-80px">
-      <div className="flex flex-1 flex-col space-y-6 px-6 lg:px-0 lg:flex-row lg:space-x-6 lg:space-y-0 lg:py-0">
+      <div className="flex flex-1 flex-col space-y-6 px-6 lg:flex-row lg:space-x-6 lg:space-y-0 lg:p-0">
         <div className="flex flex-col items-center space-y-2 text-center lg:flex-row lg:space-x-3 lg:space-y-0 lg:text-left">
           <FaMapMarkerAlt className="h-20px w-20px text-lightWhite1" />
           <a href={mermerAddressInMap} target="_blank">
