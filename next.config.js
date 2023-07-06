182f94f021a6bc6608f4739dbc635f5bb511dbae
Julian <julian890701@gmail.com>
2023-07-06 17:04:01 +0800

    change recipient

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n,
+  env: {
+    REACT_APP_MERMER_PHONE: process.env.REACT_APP_MERMER_PHONE,
+    REACT_APP_MERMER_EMAIL: process.env.REACT_APP_MERMER_EMAIL,
+    REACT_APP_MERMER_COPYRIGHT: process.env.REACT_APP_MERMER_COPYRIGHT,
+  },
 };
 
 module.exports = nextConfig;
