90eca74b50804d89f0c98c10cbc8f85997b14e95
Julian <julian890701@gmail.com>
2023-07-19 11:22:21 +0800

    author part

--- src/components/km_article/km_article.tsx
+++ src/components/km_article/km_article.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import {ImArrowRight2} from 'react-icons/im';
 import {IAuthor} from '../../interfaces/author_data';
 import {timestampToString} from '../../lib/common';
+import {useTranslation} from 'next-i18next';
+import {TranslateFunction} from '../../interfaces/locale';
 
 interface IKMArticlerops {
   title: string;
@@ -11,6 +15,8 @@ interface IKMArticlerops {
 }
 
 const KMArticle = ({title, date, content, picture, author}: IKMArticlerops) => {
+  const {t}: {t: TranslateFunction} = useTranslation('common');
+
   return (
     <div className="min-h-screen w-full font-Dosis">
       <div className="flex flex-col space-y-12 p-20">
@@ -32,10 +38,23 @@ const KMArticle = ({title, date, content, picture, author}: IKMArticlerops) => {
         </div>
       </div>
       {/* Info: (20230718 - Julian) author */}
-      <div className="relative flex h-440px w-full items-center justify-center bg-authorIntro bg-150 bg-center bg-no-repeat">
+      <div className="relative flex h-440px w-full items-center justify-center bg-authorIntro bg-150 bg-center bg-no-repeat px-20 py-10">
         {/* ToDo: (20230718 - Julian) author information */}
-        <div className="flex flex-col items-center rounded-3xl bg-glass p-12 px-20 py-10">
-          <div className="h-96px w-96px relative"></div>
+        <div className="flex flex-1 flex-col items-center space-y-4 rounded-3xl bg-glass p-12">
+          {/* Info: (20230718 - Julian) author avatar */}
+          <div className="relative flex h-96px w-96px items-center justify-center overflow-hidden rounded-full bg-lightGray2">
+            <Image src={author.avatar} fill style={{objectFit: 'cover'}} alt="author_avatar" />
+          </div>
+          {/* Info: (20230718 - Julian) author name & introduction */}
+          <div className="flex flex-col items-center">
+            <p className="text-2xl font-bold text-lightBlue1">{author.name}</p>
+            <p className="text-lg text-lightWhite1">{author.jobTitle}</p>
+          </div>
+          <p className="text-lg text-lightWhite1">{author.intro}</p>
+          <Link href={'#'} className="group flex items-center">
+            <p className="text-sm text-lightBlue1">{t('KM_DETAIL_PAGE.CHECK_AUTHOR')}</p>
+            <ImArrowRight2 className="ml-4 transition-all duration-300 ease-in-out group-hover:ml-8" />
+          </Link>
         </div>
       </div>
     </div>
--- src/pages/knowledge-management/[kmId].tsx
+++ src/pages/knowledge-management/[kmId].tsx
@@ -1,13 +1,21 @@
 import Head from 'next/head';
 import NavBar from '../../components/nav_bar/nav_bar';
 import Footer from '../../components/footer/footer';
+import Link from 'next/link';
 import KMArticle from '../../components/km_article/km_article';
+import {ImFacebook, ImTwitter, ImLinkedin2} from 'react-icons/im';
+import {FaRedditAlien} from 'react-icons/fa';
+import {RiArrowLeftSLine} from 'react-icons/ri';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
 import {ILocale} from '../../interfaces/locale';
 import {useRouter} from 'next/router';
 import {dummyKMList, notFoundKM} from '../../interfaces/km_article';
+import {useTranslation} from 'next-i18next';
+import {TranslateFunction} from '../../interfaces/locale';
+import {MERURL} from '../../constants/url';
 
 const KMDetailPage = () => {
+  const {t}: {t: TranslateFunction} = useTranslation('common');
   const router = useRouter();
   const {kmId} = router.query;
 
@@ -22,7 +30,7 @@ const KMDetailPage = () => {
 
       <NavBar />
 
-      <main className="flex w-screen flex-1 flex-col overflow-x-hidden bg-darkBlue3 py-20">
+      <main className="flex w-screen flex-1 flex-col overflow-x-hidden bg-darkBlue3 pt-20">
         {/* Info: (20230718 - Julian) Breadcrumb */}
         <div className="px-20 py-10">breadcrumb</div>
 
@@ -34,6 +42,33 @@ const KMDetailPage = () => {
           picture={kmData.picture}
           author={kmData.author}
         />
+
+        <div className="flex items-center p-20">
+          {/* ToDo: (20230719 - Julian) Share */}
+          <div className="flex flex-1 items-center space-x-12">
+            <p>{t('KM_DETAIL_PAGE.SHARE_TO')}</p>
+            <div className="flex items-center space-x-4">
+              <button className="text-2xl hover:text-lightBlue1">
+                <FaRedditAlien />
+              </button>
+              <button className="text-2xl hover:text-lightBlue1">
+                <ImFacebook />
+              </button>
+              <button className="text-2xl hover:text-lightBlue1">
+                <ImTwitter />
+              </button>
+              <button className="text-2xl hover:text-lightBlue1">
+                <ImLinkedin2 />
+              </button>
+            </div>
+          </div>
+          {/* Info: (20230719 - Julian) Back Button */}
+
+          <Link href={MERURL.KM} className="group flex items-center text-2xl hover:text-lightBlue1">
+            <RiArrowLeftSLine className="mr-2 text-2xl transition-all duration-300 ease-in-out group-hover:mr-4" />
+            <p className="text-base">{t('KM_DETAIL_PAGE.BACK_BUTTON')}</p>
+          </Link>
+        </div>
       </main>
 
       <Footer />
@@ -55,6 +90,7 @@ export const getStaticPaths = async () => {
       {params: {kmId: 'km000001'}},
       {params: {kmId: 'km000002'}},
       {params: {kmId: 'km000003'}},
+      {params: {kmId: 'km000004'}},
     ],
     fallback: 'blocking',
   };
