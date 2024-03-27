527c027b22485680f72a07031d6aeb41d01b975b
Tiny_Murky <murky0830@gmail.com>
2024-03-27 18:45:09 +0800

    add

--- src/lib/parse_form_data.ts
+++ src/lib/parse_form_data.ts
@@ -5,12 +5,13 @@ import { NextApiRequest } from "next";
 import { IncomingForm, Files, Fields, Options } from 'formidable';
 import path from "path";
 import { merMerAdminConfig } from "../constants/config";
-import fs from 'fs';
+import { promises as fs } from 'fs';
 // Helper function to wrap formidable's parse method in a promise
 export const parseForm = (req: NextApiRequest): Promise<{ fields: Fields, files: Files<string>}> => {
+  const formidableUploadUrl = process.env.VERCEL === '1' ? merMerAdminConfig.formidableUploadUrl : path.join(process.cwd(), merMerAdminConfig.formidableUploadUrl);
   const options: Partial<Options> = {
     encoding: 'utf-8',
-    uploadDir: process.env.VERCEL === '1' ? "/tmp" : path.join(process.cwd(), merMerAdminConfig.formidableUploadUrl),
+    uploadDir: formidableUploadUrl,
     keepExtensions: true,
     maxFieldsSize: 200 * 1024 * 1024, // (200mb),
     maxFields: 1000,
@@ -25,17 +26,15 @@ export const parseForm = (req: NextApiRequest): Promise<{ fields: Fields, files:
   }
 
   //Info (20240327) - Murky, Check if /tmp folder exists, if not create it
-  if (!fs.existsSync(options.uploadDir || "/tmp")) {
-    fs.mkdirSync(options.uploadDir || "/tmp");
-  }
-
-  return new Promise((resolve, reject) => {
-    const form = new IncomingForm(options);
-    form.parse(req, (err, fields, files) => {
-      if (err) {
-        return reject(err);
-      }
-      return resolve({ fields, files });
+    return new Promise(async (resolve, reject) => {
+      await fs.mkdir(formidableUploadUrl, { recursive: false }).catch();
+      const form = new IncomingForm(options);
+      form.parse(req, (err, fields, files) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve({ fields, files });
+      });
     });
-  });
+
 };
